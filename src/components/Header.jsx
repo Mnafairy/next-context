5314ca8c9db091a3f504467587b790754a4be9b4
Orgil <mnafairy@gmail.com>
2024-01-22 21:40:01 +0800

    1/22

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,3 @@
 export function Header() {
-  return <div className="w-full bg-slate-200 text-blue-600">Header</div>;
+  return <div className="w-full bg-slate-200 text-blue-600 ">Header</div>;
 }
--- src/components/Right.jsx
+++ src/components/Right.jsx
@@ -1,7 +1,15 @@
+import React from "react";
+import QuestionAnswer from "./QuestionAnswer";
+import SideInfo from "./SideInfo";
+import { useQuestionData } from "@/context/QuestionContext";
+
 export function Right() {
+  const { count, setCount } = useQuestionData();
+  console.log(count);
   return (
-    <div className="w-3/4 h-[100vh] bg-green-300">
+    <div className="w-3/4 bg-green-300 h-[100vh] flex">
       <QuestionAnswer />
+      <SideInfo />
     </div>
   );
 }
